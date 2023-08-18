584737184ad56a6eb95313d8e17edb353b44ba0b
Gideon Billings <gidobot@umich.edu>
2023-08-18 16:43:31 +1000

    Updates to docker files

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -2,8 +2,17 @@
 
 docker run --rm -it \
 	--net=host \
-	--runtime nvidia \
-	-e DISPLAY=$DISPLAY \
+	--gpus all \
+	--ipc=host \
+	--ulimit memlock=-1 \
+	--ulimit stack=67108864 \
+	--env=NVIDIA_DRIVER_CAPABILITIES=all \
+	--env=DISPLAY \
+	--env=QT_X11_NO_MITSHM=1 \
+	--device=/dev/dri:/dev/dri \
+	--pid=host \
+	--cap-add=SYS_ADMIN \
+	--cap-add=SYS_PTRACE \
 	-v /tmp/.X11-unix:/tmp/.X11-unix:rw \
 	-v $PWD/../:/depth_estimation:rw \
 	-v ~/.docker_bash_history:/root/.bash_history:rw \
